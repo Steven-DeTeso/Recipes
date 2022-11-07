a128b0c7809153886ade59445887bf9c9f7fbf66
Steven DeTeso <sadeteso@gmail.com>
2022-11-07 13:10:18 -0500

    added more set up

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,3 +1,6 @@
 from flask import Flask # type: ignore
+from flask_bcrypt import Bcrypt
 app = Flask(__name__)
-app.secret_key = " --New Text Here --"
+app.secret_key = " HoneyGirl2334445"
+
+bcrypt = Bcrypt(app)
---
+++ server.py
@@ -0,0 +1,7 @@
+from flask_app import app
+from flask_app.controllers import users
+# from flask_app.controllers import <folder name> - folder name 
+# won't be highlighted because we won't use it here, it will just be active
+
+if __name__=="__main__":
+    app.run(debug=True)
